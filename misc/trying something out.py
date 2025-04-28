03e5bb7cd5928b3313efa040329f54bd29666009
jzamzami <jzamzami@umass.edu>
2025-04-27 22:51:37 -0400

    regret is almost obliterated

--- misc/trying something out.py
+++ misc/trying something out.py
@@ -43,76 +43,65 @@ class Adversarial_Exp3:
             reward += 0
         return reward
     
-    def update(self, chosen_arm, reward):
+    def update(self, chosen_arm, reward_vector):
         probs = self.finding_probability_distributions()
         if probs[chosen_arm] > 0:
-            reward_estimate = reward / probs[chosen_arm]
+            reward_estimate = reward_vector[chosen_arm] / probs[chosen_arm]
         else:
             reward_estimate = 0
         growth_factor = math.exp((self.learning_rate / n_arms) * reward_estimate)
         self.weights[chosen_arm] *= growth_factor
 
 n_arms = 10
-n_rounds = 100000
+time_horizon = 10
 learning_rate = 0.005
 
 adversarialExp3Environment = Adversarial_Exp3(learning_rate, n_arms)
-regret = []
-cumulative_reward = 0
 
-for t in range(n_rounds):
-    chosen_arm = adversarialExp3Environment.select_arm()
+rewards_for_all_rounds = np.zeros((time_horizon, n_arms))
+for round_played in range(time_horizon):
+    rewards_for_each_round = np.zeros(n_arms)
     best_arm = adversarialExp3Environment.update_best_arm()
-    reward = adversarialExp3Environment.assign_reward(chosen_arm)
-    adversarialExp3Environment.update(chosen_arm, reward)
+    for arm in range(n_arms):
+        reward_of_arm = 0
+        if arm == best_arm:
+            reward_of_arm += 1
+        else:
+            reward_of_arm += 0
+        rewards_for_each_round[arm] = reward_of_arm
+    rewards_for_all_rounds[round_played] = rewards_for_each_round
     
-    cumulative_reward += reward
-    optimal_reward = (t + 1) * 0.7
-    regret.append(optimal_reward - cumulative_reward)
+best_arms_in_each_round = []
+for reward_vector in range(len(rewards_for_all_rounds)):
+    best_arm_in_this_round = np.argmax(rewards_for_all_rounds[reward_vector])
+    best_arms_in_each_round.append(best_arm_in_this_round)
+frequency_of_each_best_arm = np.bincount(best_arms_in_each_round)
+optimal_arm = np.argmax(frequency_of_each_best_arm)
 
-"""
-variables needed (all the code here is just pseudocode so i can think of how to find these values):
-1) reward vector -> array of arrays that contains the rewards of each arm from every round:
-    reward_vector = []
-    reward_vector_for_each_round = []
-    for round in range(time_horizon):
-        for arm in range(number_of_arms):
-            reward = get_reward
-            reward_vector_for_each_round.append(reward)
-        reward_vector.append(reward_vector_for_each_round)
-    return reward_vector
-    
-2) optimal arm -> index of arm that consistently gives us best rewards (shloon i find optimal arm from this
-reward vector):
-best_arm_in_each_round = []
-for round in range(len(reward_vector_for_each_round)):
-    max_reward_in_this_round = max(reward_vector)
-    best_arm_in_round = reward_vector_for_each_round.index(max_reward_in_this_round)
-    best_arm_in_each_round.append(best_arm_in_round)
-best_arms_overall = np.bincount(best_arm_in_each_round)
-best_arm_overall = np.argmax(best_arms_overall) #best_arm_overall would then just be our optimal arm
-return best_arm_overall
-
-3) rewards of that optimal arm -> so now that we have our optimal arm lazem we go back in time to see what rewards that optimal arm
-would have given us (and like actual regret calculation):
-cumulative_best_reward = 0
-cumulative_actual_reward = 0
+cumulative_optimal_reward = 0
+cumulative_reward = 0
 regrets = []
-for round in range(time_horizon):
-    best_reward = reward_vector_for_each_round[best_arm_overall]
-    actual_reward = reward_vector_for_each_round[arm_pulled]
-    cumulative_best_reward += best_reward
-    cumulative_actual_reward += actual_reward
-    regret_for_this_round = cumulative_best_reward - cumulative_actual_reward
-    regrets.append(regret_for_this_round)
-return regrets
-"""
 
-plt.figure(figsize=(10, 6))
-plt.plot(regret, label="Cumulative Regret")
-plt.xlabel("Round")
-plt.ylabel("Cumulative Regret")
-plt.title("Exp3 Adversarial Cumulative Regret Over Time")
-plt.legend()
-plt.grid(True)
-plt.show()
+for reward_vector in range(len(rewards_for_all_rounds)):
+    for round_played in range(len(rewards_for_all_rounds[reward_vector])):
+        chosen_arm = adversarialExp3Environment.select_arm()
+        adversarialExp3Environment.update(chosen_arm, rewards_for_all_rounds[reward_vector])
+        print(chosen_arm)
+        optimal_reward = rewards_for_each_round[optimal_arm]
+        actual_reward = rewards_for_each_round[chosen_arm]
+        
+        cumulative_optimal_reward += optimal_reward
+        cumulative_reward += actual_reward
+        
+        regret_for_this_round = cumulative_optimal_reward - cumulative_reward
+        regrets.append(regret_for_this_round)
+#print(regrets)
+
+# plt.figure(figsize=(10, 6))
+# plt.plot(regrets, label="Cumulative Regret")
+# plt.xlabel("Round")
+# plt.ylabel("Cumulative Regret")
+# plt.title("Exp3 Adversarial Cumulative Regret Over Time")
+# plt.legend()
+# plt.grid(True)
+# plt.show()
