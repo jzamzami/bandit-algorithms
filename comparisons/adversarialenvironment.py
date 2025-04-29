dd75bdc2abd5fa5ab79c18edd2ab3258741134da
jzamzami <jzamzami@umass.edu>
2025-04-28 21:49:59 -0400

    omd stochastic 3bee6

--- comparisons/adversarialenvironment.py
+++ comparisons/adversarialenvironment.py
@@ -1,7 +1,7 @@
-import numpy as np
 import math
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 
 class Adversarial_Exp3:
     def __init__(self, learning_rate, n_arms):
@@ -25,101 +25,64 @@ class Adversarial_Exp3:
         probs = self.finding_probability_distributions()
         action_chosen = np.random.choice(n_arms, p=probs)
         return action_chosen
-        
+    
     def update_best_arm(self):
         probability = random.random()
-        if probability <= 0.35:
+        if probability <= 0.45:
             best_arm = random.randint(0, n_arms - 1)
         else:
             best_arm = self.best_arm
         return best_arm
     
-    def assign_reward(self, chosen_arm):
-        best_arm = self.update_best_arm()
-        reward = 0
-        if chosen_arm == best_arm:
-            reward += 1
-        else:
-            reward += 0
-        return reward
-            
-    def update(self, chosen_arm, reward):
+    def update_arm_reward_estimate(self, chosen_arm, reward_vector):
         probs = self.finding_probability_distributions()
         if probs[chosen_arm] > 0:
-            reward_estimate = reward / probs[chosen_arm]
+            reward_estimate = reward_vector[chosen_arm] / probs[chosen_arm]
         else:
             reward_estimate = 0
         growth_factor = math.exp((self.learning_rate / n_arms) * reward_estimate)
         self.weights[chosen_arm] *= growth_factor
 
 n_arms = 10
-n_rounds = 100000
-learning_rate = 0.005
-
+time_horizon = 100000
+learning_rate = 0.01
 adversarialExp3Environment = Adversarial_Exp3(learning_rate, n_arms)
-exp3_regret = []
-cumulative_reward = 0
 
-for t in range(n_rounds):
-    chosen_arm = adversarialExp3Environment.select_arm()
+rewards_for_all_rounds = np.zeros((time_horizon, n_arms))
+for round_played in range(time_horizon):
+    rewards_for_each_round = np.zeros(n_arms)
     best_arm = adversarialExp3Environment.update_best_arm()
-    reward = adversarialExp3Environment.assign_reward(chosen_arm)
-    adversarialExp3Environment.update(chosen_arm, reward)
-    cumulative_reward += reward
-    optimal_reward = (t + 1) * 0.7
-    exp3_regret_for_this_round = optimal_reward - cumulative_reward
-    exp3_regret.append(exp3_regret_for_this_round)
-    
-def UCB(arm_means, num_arms, time_horizon, delta):
-    optimal_arm = np.argmax(arm_means)
-    num_iterations = 10
-    cumulative_regret = np.zeros([time_horizon, num_iterations])
-    for iter in range(num_iterations):
-        ucb = 100 * np.ones(num_arms)
-        emp_means = np.zeros(num_arms)
-        num_pulls = np.zeros(num_arms)
-        
-        for round in range(time_horizon):
-            greedy_arm = np.argmax(ucb)
-            reward = np.random.uniform(1, arm_means[greedy_arm])
-            num_pulls[greedy_arm] += 1 
-            cumulative_regret[round, iter] = arm_means[optimal_arm] - arm_means[greedy_arm]
-            emp_means[greedy_arm] += (reward - emp_means[greedy_arm]) / num_pulls[greedy_arm]
-            ucb[greedy_arm] = emp_means[greedy_arm] + np.sqrt(2 * np.log(1 / delta) / num_pulls[greedy_arm])
-    return cumulative_regret
-
-class AdversarialEnvironment:
-    def __init__(self, n_arms):
-        self.n_arms = n_arms
-        self.history = np.zeros(n_arms)
-        self.best_arm = random.randint(0, n_arms - 1)
-
-    def assign_reward(self, chosen_arm):
-        self.history[chosen_arm] += 1
-        if chosen_arm == self.best_arm:
-            return 1 if random.random() <= 0.7 else 0
+    for arm in range(n_arms):
+        reward_of_arm = 0
+        if arm == best_arm:
+            reward_of_arm += 1
         else:
-            return 1 if random.random() <= 0.3 else 0
-
-n_arms = 10 
-n_rounds = 100000
-delta = 0.1
-
-adversary = AdversarialEnvironment(n_arms)
-
-arm_means = np.random.uniform(0.1, 0.9, n_arms)  
-exp3 = UCB(arm_means, n_arms, n_rounds, delta)
-
-cumulative_regret = []
-cumulative_reward = 0
-
-for t in range(n_rounds):
-    chosen_arm = np.argmax(arm_means)
-    reward = adversary.assign_reward(chosen_arm)
-    cumulative_reward += reward
-    optimal_reward = (t + 1) * 0.7
-    cumulative_regret.append(optimal_reward - cumulative_reward)
+            reward_of_arm += 0
+        rewards_for_each_round[arm] = reward_of_arm
+    rewards_for_all_rounds[round_played] = rewards_for_each_round
     
+best_arms_in_each_round = []
+for reward_vector in range(len(rewards_for_all_rounds)):
+    best_arm_in_this_round = np.argmax(rewards_for_all_rounds[reward_vector])
+    best_arms_in_each_round.append(best_arm_in_this_round)
+frequency_of_each_best_arm = np.bincount(best_arms_in_each_round)
+optimal_arm = np.argmax(frequency_of_each_best_arm)
+
+cumulative_optimal_reward = 0
+cumulative_reward = 0
+exp3_regrets = []
+
+for round_reward_vector in range(len(rewards_for_all_rounds)):
+    this_rounds_reward_vector = rewards_for_all_rounds[round_reward_vector]
+    chosen_arm = adversarialExp3Environment.select_arm()
+    adversarialExp3Environment.update_arm_reward_estimate(chosen_arm, this_rounds_reward_vector)
+    optimal_reward = this_rounds_reward_vector[optimal_arm]
+    actual_reward = this_rounds_reward_vector[chosen_arm]
+    cumulative_optimal_reward += optimal_reward
+    cumulative_reward += actual_reward
+    regret_for_this_round = cumulative_optimal_reward - cumulative_reward
+    exp3_regrets.append(regret_for_this_round)
+
 class Adversarial_OMD_Environment:
     def __init__(self, learning_rate, number_of_arms):
         self.learning_rate = learning_rate
@@ -128,90 +91,101 @@ class Adversarial_OMD_Environment:
         self.number_of_arms = number_of_arms
         self.best_arm = random.randint(0, number_of_arms - 1)
     
-    def newtons_approximation_for_arm_weights(self, normalization_factor, estimated_loss_vector, learning_rate): 
+    def newtons_approximation_for_arm_weights(self, normalization_factor, estimated_loss_vector, learning_rate):
         weights_for_arms = np.zeros(number_of_arms)
         epsilon = 1.0e-9
         previous_normalization_factor = normalization_factor
         updated_normalization_factor = normalization_factor
-        for arm in range(number_of_arms):
-                    inner_product = abs((learning_rate * (estimated_loss_vector[arm] - updated_normalization_factor)))
-                    exponent_of_inner_product = math.pow(((inner_product + epsilon)), -2)
-                    weight_of_arm = 4 * exponent_of_inner_product
-                    weights_for_arms[arm] = weight_of_arm
+        while True:
+            for arm in range(number_of_arms):
+                inner_product = abs((learning_rate * (estimated_loss_vector[arm] - updated_normalization_factor)))
+                exponent_of_inner_product = math.pow(((inner_product + epsilon)), -2)
+                weight_of_arm = 4 * exponent_of_inner_product
+                weights_for_arms[arm] = weight_of_arm
+            sum_of_weights = sum(weights_for_arms)
+            numerator = sum_of_weights - 1
+            sum_of_arms_taken_to_power = 0
+            for arm_weight in range(number_of_arms):
+                updated_normalization_factor_arm_weight = math.pow(weights_for_arms[arm_weight], 3/2)
+                sum_of_arms_taken_to_power += updated_normalization_factor_arm_weight
+            denominator = (learning_rate * sum_of_arms_taken_to_power) + epsilon
+            updated_normalization_factor = previous_normalization_factor - (numerator / denominator)
+            difference_in_normalization_factors = abs(updated_normalization_factor - previous_normalization_factor)
+            previous_normalization_factor = updated_normalization_factor
+            if(difference_in_normalization_factors < epsilon):
+                break
+            else:
+                continue
         return weights_for_arms, updated_normalization_factor
-    
-    def normalizingWeights(self, weights_for_arms):
-        weights_of_arms, self.normalization_factor = self.newtons_approximation_for_arm_weights(self.normalization_factor, self.estimated_loss_vector, self.learning_rate)
-        sum_of_weights = sum(weights_of_arms)
-        for arm_weight in range(number_of_arms):
-            normalized_arm_weight = weights_of_arms[arm_weight] / sum_of_weights
-            weights_of_arms[arm_weight] = normalized_arm_weight
-        return weights_of_arms
 
     def selectArm(self):
         weights_of_arms, self.normalization_factor = self.newtons_approximation_for_arm_weights(self.normalization_factor, self.estimated_loss_vector, self.learning_rate)
-        normalized_weights = self.normalizingWeights(weights_of_arms)
-        action_chosen = np.random.choice(number_of_arms, p=normalized_weights)
+        action_chosen = np.random.choice(number_of_arms, p = weights_of_arms)
         return action_chosen
     
     def update_best_arm(self):
         probability = random.random()
-        if probability < 0.3:
+        if probability < 0.7:
             best_arm = random.randint(0, number_of_arms - 1)
         else:
             best_arm = self.best_arm
         return best_arm
     
-    def getLoss(self, chosen_arm):
-        best_arm = self.update_best_arm()
-        probability = random.random()
-        loss = 0
-        if chosen_arm == best_arm:
-            if probability < 0.7:
-                loss += 1
-            else:
-                loss += 0
-        else:
-            if probability < 0.3:
-                loss += 1
-            else:
-                loss += 0
-        return loss
-    
-    def updateLossVector(self, chosen_arm, loss):
+    def updateLossVector(self, chosen_arm, loss_vector):
         weights_of_arms, self.normalization_factor = self.newtons_approximation_for_arm_weights(self.normalization_factor, self.estimated_loss_vector, self.learning_rate)
-        normalized_weights = self.normalizingWeights(weights_of_arms)
-        if normalized_weights[chosen_arm] > 0:
-            new_loss_estimate = loss / normalized_weights[chosen_arm]
+        if weights_of_arms[chosen_arm] > 0:
+            new_loss_estimate = loss_vector[chosen_arm] / weights_of_arms[chosen_arm]
         else:
             new_loss_estimate = 0
         self.estimated_loss_vector[chosen_arm] += new_loss_estimate
 
-learning_rate = 0.005
 number_of_arms = 10
 time_horizon = 100000
-simulations = 1
+learning_rate = 0.005
 
-for simulation in range(simulations):
-    omd_adversarial = Adversarial_OMD_Environment(learning_rate, number_of_arms)
-    regrets = []
-    cumulative_loss = 0
+adversarialExp3Environment = Adversarial_OMD_Environment(learning_rate, number_of_arms)
 
-    for round_played in range(time_horizon):
-        chosen_arm = omd_adversarial.selectArm()
-        best_arm = omd_adversarial.update_best_arm()
-        loss = omd_adversarial.getLoss(chosen_arm)
-        cumulative_loss += loss
-        omd_adversarial.updateLossVector(chosen_arm, loss)
-        optimal_loss = (round_played + 1) * 0.3
-        regrets.append(cumulative_loss - optimal_loss)
+losses_for_all_rounds = np.zeros((time_horizon, number_of_arms))
+for round_played in range(time_horizon):
+    losses_for_each_round = np.zeros(number_of_arms)
+    best_arm = adversarialExp3Environment.update_best_arm()
+    for arm in range(number_of_arms):
+        loss_of_arm = 0
+        if arm == best_arm:
+            loss_of_arm += 0
+        else:
+            loss_of_arm += 1
+        losses_for_each_round[arm] = loss_of_arm
+    losses_for_all_rounds[round_played] = losses_for_each_round
     
-#plt.plot(cumulative_regret, label="ucb_regret", color = "green")
-plt.plot(exp3_regret, label="exp3_regret", color ="red")
-plt.plot(regrets, label="omd_regret", color ="blue")
+best_arms_in_each_round = []
+for loss_vector in range(len(losses_for_all_rounds)):
+    best_arm_in_this_round = np.argmin(losses_for_all_rounds[loss_vector])
+    best_arms_in_each_round.append(best_arm_in_this_round)
+frequency_of_each_best_arm = np.bincount(best_arms_in_each_round)
+optimal_arm = np.argmax(frequency_of_each_best_arm)
+
+cumulative_optimal_loss = 0
+cumulative_loss = 0
+regrets = []
+
+for round_loss_vector in range(len(losses_for_all_rounds)):
+    this_rounds_loss_vector = losses_for_all_rounds[round_loss_vector]
+    chosen_arm = adversarialExp3Environment.selectArm()
+    adversarialExp3Environment.updateLossVector(chosen_arm, this_rounds_loss_vector)
+    optimal_loss = this_rounds_loss_vector[optimal_arm]
+    actual_loss = this_rounds_loss_vector[chosen_arm]
+    cumulative_optimal_loss += optimal_loss
+    cumulative_loss += actual_loss
+    regret_for_this_round = cumulative_loss - cumulative_optimal_loss
+    regrets.append(regret_for_this_round)
+
+plt.figure(figsize=(10, 6))
+plt.plot(exp3_regrets, label="Exp3 Cumulative Regret", color = "red")
+plt.plot(regrets, label="OMD Cumulative Regret", color = "blue")
 plt.xlabel("Round")
-plt.ylabel('Cumulative Regret')
-plt.title("Adversarial Cumulative Regret Comparison")
+plt.ylabel("Cumulative Regret")
+plt.title("Exp3 vs OMD Adversarial Cumulative Regret")
 plt.legend()
-plt.grid()
+plt.grid(True)
 plt.show()
--- comparisons/stochasticenvironment.py
+++ comparisons/stochasticenvironment.py
@@ -2,151 +2,10 @@ import math
 import matplotlib.pyplot as plt
 import random
 import numpy as np
-        
-class stochastic_OMD_Environment:
-    def __init__(self, learning_rate, number_of_arms):
-        self.learning_rate = learning_rate
-        self.normalization_factor = 200*math.sqrt(10)
-        self.estimated_loss_vector = np.zeros(number_of_arms)
-        self.number_of_arms = number_of_arms
-        self.arm_means = np.random.binomial(0.1, 0.9, number_of_arms)
-    
-    def newtons_approximation_for_arm_weights(self, normalization_factor, estimated_loss_vector, learning_rate):
-        weights_for_arms = np.zeros(number_of_arms)
-        epsilon = 1.0e-9
-        previous_normalization_factor = normalization_factor
-        updated_normalization_factor = normalization_factor
-        for arm in range(number_of_arms):
-                    inner_product = abs((learning_rate * (estimated_loss_vector[arm] - updated_normalization_factor)))
-                    exponent_of_inner_product = math.pow(((inner_product + epsilon)), -2)
-                    weight_of_arm = 4 * exponent_of_inner_product
-                    weights_for_arms[arm] = weight_of_arm
-        return weights_for_arms, updated_normalization_factor
-    
-    def normalizingWeights(self, weights_for_arms):
-        weights_of_arms, self.normalization_factor = self.newtons_approximation_for_arm_weights(self.normalization_factor, self.estimated_loss_vector, self.learning_rate)
-        sum_of_weights = sum(weights_of_arms)
-        for arm_weight in range(number_of_arms):
-            normalized_arm_weight = weights_of_arms[arm_weight] / sum_of_weights
-            weights_of_arms[arm_weight] = normalized_arm_weight
-        return weights_of_arms
-
-    def selectArm(self):
-        weights_of_arms, self.normalization_factor = self.newtons_approximation_for_arm_weights(self.normalization_factor, self.estimated_loss_vector, self.learning_rate)
-        normalized_weights = self.normalizingWeights(weights_of_arms)
-        action_chosen = np.random.choice(number_of_arms, p=normalized_weights)
-        return action_chosen
-    
-    def getLoss(self, chosen_arm):
-        best_arm = np.argmax(self.arm_means)
-        probablity = random.random()
-        loss = 0
-        if chosen_arm == best_arm:
-            if probablity < 0.7:
-                loss += 1
-            else:
-                loss += 0
-        else:
-            if probablity < 0.3:
-                loss += 1
-            else:
-                loss += 0
-        return loss
-    
-    def updateLossVector(self, chosen_arm, loss):
-        weights_of_arms, self.normalization_factor = self.newtons_approximation_for_arm_weights(self.normalization_factor, self.estimated_loss_vector, self.learning_rate)
-        normalized_weights = self.normalizingWeights(weights_of_arms)
-        if normalized_weights[chosen_arm] > 0:
-            new_loss_estimate = loss / normalized_weights[chosen_arm]
-        else:
-            new_loss_estimate = 0
-        self.estimated_loss_vector[chosen_arm] += new_loss_estimate
-
-learning_rate = 0.005
-number_of_arms = 10
-time_horizon = 100000
-simulations = 1
-
-for simulation in range(simulations):
-    omd_stochastic = stochastic_OMD_Environment(learning_rate, number_of_arms)
-    omd_regret = []
-    cumulative_loss = 0
-
-    for round_played in range(time_horizon):
-        chosen_arm = omd_stochastic.selectArm()
-        loss = omd_stochastic.getLoss(chosen_arm)
-        cumulative_loss += loss
-        omd_stochastic.updateLossVector(chosen_arm, loss)
-        optimal_loss = (round_played + 1) * 0.3
-        omd_regret.append(cumulative_loss - optimal_loss)
-        
-class stochastic_Exp3:
-    def __init__(self, learning_rate, n_arms):
-        self.learning_rate = learning_rate
-        self.n_arms = n_arms
-        self.weights = np.ones(n_arms)
-        self.arm_means = np.random.binomial(0.1, 0.9, n_arms)
-
-    def finding_probability_distributions(self):
-        n_arms = len(self.weights)
-        total_weight = sum(self.weights)
-        probs = np.zeros(n_arms)
-        for arm in range(self.n_arms):
-            first_term = (1 - self.learning_rate) * (self.weights[arm] / total_weight)
-            second_term = (self.learning_rate / n_arms)
-            update_rule_for_arm = first_term + second_term
-            probs[arm] = update_rule_for_arm
-        return probs
-    
-    def select_arm(self):
-        probs = self.finding_probability_distributions()
-        action_chosen = np.random.choice(n_arms, p=probs)
-        return action_chosen
-    
-    def assign_reward(self, chosen_arm):
-        best_arm = np.argmax(self.arm_means)
-        probablity = random.random()
-        reward = 0
-        if chosen_arm == best_arm:
-            if probablity < 0.7:
-                reward += 1
-            else:
-                reward += 0
-        else:
-            if probablity < 0.3:
-                reward += 1
-            else:
-                reward += 0
-        return reward
-    
-    def update(self, chosen_arm, reward):
-        probs = self.finding_probability_distributions()
-        if probs[chosen_arm] > 0:
-            reward_estimate = reward / probs[chosen_arm]
-        else:
-            reward_estimate = 0
-        growth_factor = math.exp((self.learning_rate / n_arms) * reward_estimate)
-        self.weights[chosen_arm] *= growth_factor
-
-n_arms = 10
-n_rounds = 100000
-learning_rate = 0.01
-
-stochasticExp3Environment = stochastic_Exp3(learning_rate, n_arms)
-regret = []
-cumulative_reward = 0
-
-for t in range(n_rounds):
-    chosen_arm = stochasticExp3Environment.select_arm()
-    reward = stochasticExp3Environment.assign_reward(chosen_arm)
-    stochasticExp3Environment.update(chosen_arm, reward)
-    cumulative_reward += reward
-    optimal_reward = (t + 1) * 0.7
-    regret.append(optimal_reward - cumulative_reward) 
 
 def UCB(arm_means, num_arms, time_horizon, delta):
     optimal_arm = np.argmax(arm_means)
-    num_iterations = 30
+    num_iterations = 1
     regret = np.zeros([time_horizon, num_iterations])
     for iter in range(num_iterations):
         ucb = 100 * np.ones(num_arms)
@@ -160,20 +19,120 @@ def UCB(arm_means, num_arms, time_horizon, delta):
             emp_means[greedy_arm] += (reward - emp_means[greedy_arm]) / num_pulls[greedy_arm]
             ucb[greedy_arm] = emp_means[greedy_arm] + np.sqrt(2 * np.log(1 / delta) / num_pulls[greedy_arm])
     return regret
-
 num_arms = 10
 time_horizon = 100000
 arm_means = np.random.uniform(0.1, 0.9, num_arms)
 delta = 0.1
 regret = UCB(arm_means, num_arms, time_horizon, delta)  
-cumulative_regret = np.cumsum(np.mean(regret, axis=1))
 
-#plt.plot(regret, label="exp3_regret", color = "red")
-plt.plot(omd_regret, label='omd_regret', color = "blue")
-plt.plot(cumulative_regret, label="ucb_regret", color = "green")
+ucb_cumulative_regret = np.cumsum(np.mean(regret, axis=1))
+
+class stochastic_OMD_Environment:
+    def __init__(self, learning_rate, number_of_arms):
+        self.learning_rate = learning_rate
+        self.normalization_factor = 200*math.sqrt(10)
+        self.estimated_loss_vector = np.zeros(number_of_arms)
+        self.number_of_arms = number_of_arms
+        self.best_arm = random.randint(0, number_of_arms - 1)
+    
+    def newtons_approximation_for_arm_weights(self, normalization_factor, estimated_loss_vector, learning_rate):
+        weights_for_arms = np.zeros(number_of_arms)
+        epsilon = 1.0e-9
+        previous_normalization_factor = normalization_factor
+        updated_normalization_factor = normalization_factor
+        while True:
+            for arm in range(number_of_arms):
+                inner_product = abs((learning_rate * (estimated_loss_vector[arm] - updated_normalization_factor)))
+                exponent_of_inner_product = math.pow(((inner_product + epsilon)), -2)
+                weight_of_arm = 4 * exponent_of_inner_product
+                weights_for_arms[arm] = weight_of_arm
+            sum_of_weights = sum(weights_for_arms)
+            numerator = sum_of_weights - 1
+            sum_of_arms_taken_to_power = 0
+            for arm_weight in range(number_of_arms):
+                updated_normalization_factor_arm_weight = math.pow(weights_for_arms[arm_weight], 3/2)
+                sum_of_arms_taken_to_power += updated_normalization_factor_arm_weight
+            denominator = (learning_rate * sum_of_arms_taken_to_power) + epsilon
+            updated_normalization_factor = previous_normalization_factor - (numerator / denominator)
+            difference_in_normalization_factors = abs(updated_normalization_factor - previous_normalization_factor)
+            previous_normalization_factor = updated_normalization_factor
+            if(difference_in_normalization_factors < epsilon):
+                break
+            else:
+                continue
+        return weights_for_arms, updated_normalization_factor
+
+    def selectArm(self):
+        weights_of_arms, self.normalization_factor = self.newtons_approximation_for_arm_weights(self.normalization_factor, self.estimated_loss_vector, self.learning_rate)
+        action_chosen = np.random.choice(number_of_arms, p = weights_of_arms)
+        return action_chosen
+    
+    def update_best_arm(self):
+        return self.best_arm
+    
+    def updateLossVector(self, chosen_arm, loss_vector):
+        weights_of_arms, self.normalization_factor = self.newtons_approximation_for_arm_weights(self.normalization_factor, self.estimated_loss_vector, self.learning_rate)
+        if weights_of_arms[chosen_arm] > 0:
+            new_loss_estimate = loss_vector[chosen_arm] / weights_of_arms[chosen_arm]
+        else:
+            new_loss_estimate = 0
+        self.estimated_loss_vector[chosen_arm] += new_loss_estimate
+
+number_of_arms = 10
+time_horizon = 100000
+learning_rate = 0.008
+#this one is shwya 3bee6 mdri leh 
+
+stochasticExp3Environment = stochastic_OMD_Environment(learning_rate, number_of_arms)
+best_arm = stochasticExp3Environment.update_best_arm()
+
+losses_for_all_rounds = np.zeros((time_horizon, number_of_arms))
+for round_played in range(time_horizon):
+    losses_for_each_round = np.zeros(number_of_arms)
+    for arm in range(number_of_arms):
+        loss_of_arm = 0
+        probability = random.random()
+        if arm == best_arm:
+            if probability < 0.8:
+                loss_of_arm += 0
+            else:
+                loss_of_arm += 1
+        else:
+            if probability < 0.2:
+                loss_of_arm += 0
+            else:
+                loss_of_arm += 1
+        losses_for_each_round[arm] = loss_of_arm
+    losses_for_all_rounds[round_played] = losses_for_each_round
+    
+best_arms_in_each_round = []
+for loss_vector in range(len(losses_for_all_rounds)):
+    best_arm_in_this_round = np.argmin(losses_for_all_rounds[loss_vector])
+    best_arms_in_each_round.append(best_arm_in_this_round)
+frequency_of_each_best_arm = np.bincount(best_arms_in_each_round)
+optimal_arm = np.argmax(frequency_of_each_best_arm)
+
+cumulative_optimal_loss = 0
+cumulative_loss = 0
+regrets = []
+
+for round_loss_vector in range(len(losses_for_all_rounds)):
+    this_rounds_loss_vector = losses_for_all_rounds[round_loss_vector]
+    chosen_arm = stochasticExp3Environment.selectArm()
+    stochasticExp3Environment.updateLossVector(chosen_arm, this_rounds_loss_vector)
+    optimal_loss = this_rounds_loss_vector[optimal_arm]
+    actual_loss = this_rounds_loss_vector[chosen_arm]
+    cumulative_optimal_loss += optimal_loss
+    cumulative_loss += actual_loss
+    regret_for_this_round = cumulative_loss - cumulative_optimal_loss
+    regrets.append(regret_for_this_round)
+
+plt.figure(figsize=(10, 6))
+plt.plot(ucb_cumulative_regret, label="UCB Cumulative Regret", color = "green")
+plt.plot(regrets, label="OMD Cumulative Regret", color = "blue")
 plt.xlabel("Round")
-plt.ylabel('Cumulative Regret')
-plt.title("Stochastic Cumulative Regret Comparison")
+plt.ylabel("Cumulative Regret")
+plt.title("UCB vs OMD Stochastic Cumulative Regret Over Time")
 plt.legend()
-plt.grid()
+plt.grid(True)
 plt.show()
